1110659bc685710700c6fe960067ecb844a9f93c
Simon Brungs <00pflaume@gmail.com>
2016-10-03 22:16:33 +0200

    First upload

---
+++ src/main/java/de/simonsator/partyandfriends/partytoggle/PTMain.java
@@ -0,0 +1,14 @@
+package de.simonsator.partyandfriends.partytoggle;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * @author simonbrungs
+ * @version 1.0.0 03.10.16
+ */
+public class PTMain extends Plugin {
+	@Override
+	public void onEnable() {
+
+	}
+}
