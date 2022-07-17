e4c956a6841f185fee3e798e46c5c007b84fbff0
Simonsator <00pflaume@gmail.com>
2022-07-17 23:15:23 +0200

    Fixed loading plugin

--- src/main/java/de/simonsator/partyandfriends/partytoggle/velocity/PTMain.java
+++ src/main/java/de/simonsator/partyandfriends/partytoggle/velocity/PTMain.java
@@ -1,6 +1,7 @@
 package de.simonsator.partyandfriends.partytoggle.velocity;
 
 import de.simonsator.partyandfriends.velocity.api.PAFExtension;
+import de.simonsator.partyandfriends.velocity.main.Main;
 import de.simonsator.partyandfriends.velocity.party.command.PartyCommand;
 
 import java.io.File;
@@ -8,8 +9,10 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class PTMain extends PAFExtension {
+
 	public PTMain(Path folder) {
 		super(folder);
+		onEnable();
 	}
 
 	@Override
@@ -17,8 +20,8 @@ public class PTMain extends PAFExtension {
 		try {
 			PTConfig config = (new PTConfig(new File(getConfigFolder(), "config.yml"), this));
 			ChatManager chatManager = new ChatManager();
-			getAdapter().registerListener(chatManager, this);
-			getAdapter().registerListener(new RemoveManager(chatManager), this);
+			getAdapter().registerListener(chatManager, Main.getInstance());
+			getAdapter().registerListener(new RemoveManager(chatManager), Main.getInstance());
 			PartyCommand.getInstance().addCommand(
 					new PartyToggle(config.getStringList("Names"),
 							config.getInt("Priority"), config.getString("Messages.Help"), chatManager, config));
